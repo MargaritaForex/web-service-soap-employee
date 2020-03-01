fafbdfdba6f20cee7ef4a611eb0c248c078479da
erika forero <ingerika.forero@gmail.com>
2020-02-29 20:25:43 -0500

    EF-creation of xsd and modification pom.xml

---
+++ src/main/java/soap/employee/EmployeeRequest.java
@@ -0,0 +1,262 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2020.02.29 a las 08:16:26 PM COT 
+//
+
+
+package soap.employee;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="full_name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="last_name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="document_type" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="document_number" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="date_birth" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *         &lt;element name="company_entry_date" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *         &lt;element name="role" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="salary" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "fullName",
+    "lastName",
+    "documentType",
+    "documentNumber",
+    "dateBirth",
+    "companyEntryDate",
+    "role",
+    "salary"
+})
+@XmlRootElement(name = "EmployeeRequest")
+public class EmployeeRequest {
+
+    @XmlElement(name = "full_name", required = true)
+    protected String fullName;
+    @XmlElement(name = "last_name", required = true)
+    protected String lastName;
+    @XmlElement(name = "document_type", required = true)
+    protected String documentType;
+    @XmlElement(name = "document_number", required = true)
+    protected String documentNumber;
+    @XmlElement(name = "date_birth", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar dateBirth;
+    @XmlElement(name = "company_entry_date", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar companyEntryDate;
+    @XmlElement(required = true)
+    protected String role;
+    protected double salary;
+
+    /**
+     * Obtiene el valor de la propiedad fullName.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Define el valor de la propiedad fullName.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFullName(String value) {
+        this.fullName = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad lastName.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Define el valor de la propiedad lastName.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad documentType.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    /**
+     * Define el valor de la propiedad documentType.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDocumentType(String value) {
+        this.documentType = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad documentNumber.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    /**
+     * Define el valor de la propiedad documentNumber.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDocumentNumber(String value) {
+        this.documentNumber = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad dateBirth.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDateBirth() {
+        return dateBirth;
+    }
+
+    /**
+     * Define el valor de la propiedad dateBirth.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDateBirth(XMLGregorianCalendar value) {
+        this.dateBirth = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad companyEntryDate.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getCompanyEntryDate() {
+        return companyEntryDate;
+    }
+
+    /**
+     * Define el valor de la propiedad companyEntryDate.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setCompanyEntryDate(XMLGregorianCalendar value) {
+        this.companyEntryDate = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad role.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * Define el valor de la propiedad role.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRole(String value) {
+        this.role = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad salary.
+     * 
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * Define el valor de la propiedad salary.
+     * 
+     */
+    public void setSalary(double value) {
+        this.salary = value;
+    }
+
+}
